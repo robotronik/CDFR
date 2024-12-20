e7ec777872516c3da9db1f7fb1b7653d4ff2351e
Ludovic Bouchard <ludovicb1239@gmail.com>
2024-12-20 14:24:54 +0100

    Updated highways and repos

---
+++ stategie/2025/obj_to_highways.py
@@ -0,0 +1,52 @@
+# A python program to convert .obj files into arrays to copy paste into cpp files
+# OBJ points are stored with format [i] = {x,y}
+# OBJ lines are stored with format [k] = {i,j}
+
+# Blender settings:
+# - Export as Wavefront (.obj)
+# - Selection Only
+# - triangulate faces
+
+
+import sys
+import os
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python obj_to_highways.py <filename>")
+        return
+
+    filename = sys.argv[1]
+    if not os.path.isfile(filename):
+        print("File not found")
+        return
+
+    obj_file = open(filename, "r")
+    f_lines = obj_file.readlines()
+    obj_file.close()
+
+    points = []
+    lines = []
+
+    for f_line in f_lines:
+        if f_line.startswith("v"):
+            point = f_line.split()
+            points.append((round(float(point[1])), round(float(point[2]))))
+        elif f_line.startswith("l"):
+            line = f_line.split()
+            lines.append((int(line[1])-1, int(line[2])-1))
+ 
+    print("#define HIGHWAY_POINTS_COUNT " + str(len(points)))
+    print("#define HIGHWAY_LINES_COUNT " + str(len(lines)))
+    #print without line return
+    print("static highway_point points[] = {", end="")
+    for point in points:
+        print("{" + str(point[0]) + ", " + str(point[1]) + "},", end="")
+    print("};")
+    print("static highway_line lines[] = {", end="")
+    for line in lines:
+        print("{" + str(line[0]) + ", " + str(line[1]) + "},", end="")
+    print("};")
+
+if __name__ == "__main__":
+    main()
